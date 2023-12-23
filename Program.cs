6f014e155f4192b091e078ad073dae2b6e96be50
breno085 <br3noacioli@gmail.com>
2023-12-23 19:02:18 -0300

    implementei o menu interativo

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿string opcao;
+bool exibirMenu = true;
+
+do
+{   
+    Console.Clear();
+    Console.WriteLine("Digite a sua opção:");
+    Console.WriteLine("1 - Cadastrar cliente");
+    Console.WriteLine("2 - Buscar cliente");
+    Console.WriteLine("3 - Apagar cliente");
+    Console.WriteLine("4 - Encerrar");
+    
+    opcao = Console.ReadLine();
+
+    switch(opcao)
+    {
+        case "1":
+        Console.WriteLine("tbd");
+        break;
+
+        case "2":
+        Console.WriteLine("tbd");
+        break;
+
+        case "3":
+        Console.WriteLine("tbd");
+        break;
+
+        case "4":
+        // Environment.Exit(0); //obs: isso terminar a exucução do programa, não só do loop
+        exibirMenu = false;
+        break;
+
+        default:
+        Console.WriteLine("tbd");
+        break;
+    }
+    Console.WriteLine("Pressione enter para continuar");
+    Console.ReadLine();
+} while (exibirMenu);
