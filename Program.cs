a3c02aa8f2d9435679ff4a1008075cfa3b32cf26
breno085 <br3noacioli@gmail.com>
2024-01-05 23:19:58 -0300

    versao 1 - sem usar oop

--- Program.cs
+++ Program.cs
@@ -1,13 +1,27 @@
-﻿string opcao;
+﻿using System.Globalization;
+CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("pt-BR");
+
+string opcao;
 bool exibirMenu = true;
+decimal precoInicial;
+decimal precoPorHora;
+List<string> placasDeVeiculos = new List<string>();
+
+Console.WriteLine("Seja bem vindo ao sistema de estacionamento!");
+
+Console.WriteLine("Digite o preço inicial:");
+precoInicial = decimal.Parse(Console.ReadLine());
+
+Console.WriteLine("Agora digite o preço pro hora:");
+precoPorHora = decimal.Parse(Console.ReadLine());
 
 do
 {   
     Console.Clear();
     Console.WriteLine("Digite a sua opção:");
-    Console.WriteLine("1 - Cadastrar cliente");
-    Console.WriteLine("2 - Buscar cliente");
-    Console.WriteLine("3 - Apagar cliente");
+    Console.WriteLine("1 - Cadastrar veículo");
+    Console.WriteLine("2 - Remover veículo");
+    Console.WriteLine("3 - Listar veículos");
     Console.WriteLine("4 - Encerrar");
     
     opcao = Console.ReadLine();
@@ -15,15 +29,15 @@ do
     switch(opcao)
     {
         case "1":
-        Console.WriteLine("tbd");
+        CadastrarVeiculo();
         break;
 
         case "2":
-        Console.WriteLine("tbd");
+        RemoverVeiculos();
         break;
 
         case "3":
-        Console.WriteLine("tbd");
+        ListarVeiculos();
         break;
 
         case "4":
@@ -37,4 +51,55 @@ do
     }
     Console.WriteLine("Pressione enter para continuar");
     Console.ReadLine();
+
 } while (exibirMenu);
+
+void CadastrarVeiculo()
+{   
+    string placaVeiculo;
+    
+    Console.WriteLine("Digite a placa do veículo para estacionar:");
+    placaVeiculo = Console.ReadLine();
+    
+    placasDeVeiculos.Add(placaVeiculo);
+}
+
+void ListarVeiculos()
+{
+    foreach(string item in placasDeVeiculos)
+    {
+        Console.WriteLine(item);
+    }
+}
+
+void RemoverVeiculos()
+{
+    string placaRemovida;
+
+    do
+    {
+    Console.WriteLine("Digite a placa do veículo para remover");
+    placaRemovida = Console.ReadLine();
+
+    if (!placasDeVeiculos.Contains(placaRemovida))
+    {
+        Console.WriteLine("Digite uma placa válida!");
+    } 
+    else
+    {
+        placasDeVeiculos.Remove(placaRemovida);
+
+        Console.WriteLine("Digite a quantidade de horas que o veículo ficou estacionado:");
+        double horas = double.Parse(Console.ReadLine());
+
+        Console.WriteLine($"O Veículo {placaRemovida} foi removido e o preço total "
+        + $"foi de: {precoTotal(horas, precoInicial, precoPorHora):C}");
+    }
+
+    } while (placasDeVeiculos.Contains(placaRemovida));
+}
+
+decimal precoTotal(double horas, decimal precoInicial, decimal precoPorHora)
+{
+    return precoInicial + (decimal) horas*precoPorHora;
+}
